e7361e2119ef80e77d9d2a593825eecb3199c4e0
Yevheniia2 <Veintq2291244@gmail.com>
2022-07-09 15:43:13 +0300

    commit_7

--- src/getPixabay.js
+++ src/getPixabay.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 const API_KEY = '28465620-b99d2f18707b881a5a8144a5c';
-// key - твой уникальный ключ доступа к API.
-// q - термин для поиска. То, что будет вводить пользователь.
-// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
-// orientation - ориентация фотографии. Задай значение horizontal.
-// safesearch - фильтр по возрасту. Задай значение true.
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['key'] = API_KEY;
 
 export class GetPixabayApi {
     constructor() {
         this.searchQuery = '';
+        this.page = 1;
     }
     async fetchImages() {
         const params = new URLSearchParams({
@@ -20,10 +15,11 @@ export class GetPixabayApi {
             image_type: 'photo',
             orientation: 'horizontal',
             safesearch: true,
-            page: 1,
+            page: this.page,
             per_page: 40,
         });
         const { data } = await axios.get(`?${params}`);
+        this.incrementPage();
         return data;
     }
     get searchQuery1() {
@@ -31,5 +27,11 @@ export class GetPixabayApi {
     }
     set searchQuery1(newSearchQuery) {
         this.searchQuery = newSearchQuery;
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import InfiniteScroll from 'infinite-scroll';
 import { GetPixabayApi } from './getPixabay.js';
+import axios from "axios";
 
 Notify.init ({
     position: 'center-top',
@@ -13,11 +14,18 @@ Notify.init ({
 
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('.search-form');
-const simpleLightbox = new SimpleLightbox('.gallery a');
+const loadMoreBtnRef = document.querySelector('.load-more');
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 300,
+});
+const getPixabayApi = new GetPixabayApi();
+
+const API_KEY = '28465620-b99d2f18707b881a5a8144a5c';
 
 formRef.addEventListener('submit', onFormSubmit);
-
-const getPixabayApi = new GetPixabayApi();
+loadMoreBtnRef.addEventListener('click', onLoadMoreBtnClick);
 
 function makeGalleryMarkup(searchedImages) {
     return searchedImages.map(({
@@ -51,41 +59,82 @@ function makeGalleryMarkup(searchedImages) {
 
 function renderGallery(searchedImages) {
     galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkup(searchedImages));
-    simpleLightbox.refresh();
 }
 
 async function onFormSubmit(evt) {
     evt.preventDefault();
     clearGalleryMarkup();
+    getPixabayApi.resetPage();
+    const infScroll = new InfiniteScroll(galleryRef, {
+        path: function () {
+            const params = `?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=truekk&page=${this.page}&per_page=${40}`;
+            return axios.defaults.baseURL + params;
+        },
+        scrollThreshold: 0,
+        responseBody: 'json',
+        status: '.page-load-status',
+        history: false,
+        });
+        infScroll.on('load', function (body) {
+        dataCheckerAndRender(body);
+    });
+    infScroll.pageIndex = 1;
     const request = evt.target.elements.searchQuery.value.trim();
     if(!request) return Notify.info('Please, enter something for search');
     GetPixabayApi.searchQuery1 = request;
-    const { hits, totalHits } = await getPixabayApi.fetchImages();
-    if(!totalHits) {
-        return Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+    try{
+        const { hits, totalHits } = await getPixabayApi.fetchImages();
+        if(!totalHits) {
+            return Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+        }
+        Notify.success(`Hooray! We found ${totalHits} images.`);
+        renderGallery(hits);
+        simpleLightbox.refresh();
+    } catch(error) {
+        console.log(error.message);
     }
-    Notify.success(`Hooray! We found ${totalHits} images.`)
-    renderGallery(hits);
     evt.target.reset();
+    infScroll.loadNextPage();
+}
+
+async function onLoadMoreBtnClick() {
+    try{
+    const { hits, totalHits } = await getPixabayApi.fetchImages();
+    renderGallery(hits);
+    simpleLightbox.refresh();
+    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+});
+    } catch(error) {
+        console.log(error.message);
+    }
 }
 
 function clearGalleryMarkup() {
     galleryRef.innerHTML = '';
 }
 
-// webformatURL - ссылка на маленькое изображение для списка карточек.
-// largeImageURL - ссылка на большое изображение.
-// tags - строка с описанием изображения. Подойдет для атрибута alt.
-// likes - количество лайков.
-// views - количество просмотров.
-// comments - количество комментариев.
-// downloads - количество загрузок.
-
-// const { height: cardHeight } = document
-//     .querySelector(".gallery")
-//     .firstElementChild.getBoundingClientRect();
-
-// window.scrollBy({
-//     top: cardHeight * 2,
-//     behavior: "smooth",
-// });
+function dataCheckerAndRender(data) {
+    page = infScroll.pageIndex - 1;
+    console.log(page);
+    totalPages = Math.ceil(data.totalHits / per_page);
+    // if (data.hits.length === 0) {
+    //     Notify.failure(
+    //     'Sorry, there are no images matching your search query. Please try again.'
+    //     );
+    //     return;
+    // }
+    // if (page === 1) {
+    //     Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    //     renderGallery(data);
+    //     return;
+    // }
+    if (page === totalPages) {
+        Notify.info("We're sorry, but you've reached the end of search results.");
+        renderGallery(data);
+        return;
+    }
+    renderGallery(data);
+}
